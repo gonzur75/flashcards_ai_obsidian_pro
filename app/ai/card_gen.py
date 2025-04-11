8bb217a8dd4965afa53f93bfe61d09274429925c
Marcin Włodarczyk <gonzur75@gmail.com>
2025-04-11 12:15:40 +0200

    refactor

--- app/ai/card_gen.py
+++ app/ai/card_gen.py
@@ -1,4 +1,4 @@
-import logging
+import logging.config
 import os
 from pathlib import Path
 
@@ -9,29 +9,29 @@ from app.tools.open_file import open_file
 
 logger = logging.getLogger(__name__)
 
+
 class CardGen:
     system_prompt_path: Path = Path(__file__).parent.resolve() / "prompts"
 
     def __new__(cls, *args, **kwargs):
         dotenv.load_dotenv(os.path.join(os.path.dirname(__file__), "..", "..", ".env", ".env-chatgpt"))
-        if os.getenv("OPEN_API_KEY") is None:
+        if os.getenv("OPENAI_API_KEY") is None:
             raise ValueError("OPEN_API_KEY is not set in .env file")
         return super().__new__(cls)
 
-    def __init__(self, model="gpt-3.5-turbo", response_format=None):
+    def __init__(self, model: str = "gpt-3.5-turbo", response_format: dict[str, str] = None) -> None:
         self.response_format = response_format or {"type": "json_object"}
-        self.client = OpenAI()
+        self.client: OpenAI = OpenAI()
         self.model = model
 
-    def process_flashcard(self, user_prompt: str):
+    def create_card_json(self, user_prompt: str):
         messages = [
-            {"role": "system", "content": open_file(type(self).system_prompt_path / "system_prompt.txt")},
-            {"role": "user", "content": user_prompt},
+            {"role": "system", "content": open_file(type(self).system_prompt_path / "systems_prompt.txt")},
+            {"role": "user", "content": "Provide a JSON response with the following data: " + user_prompt},
         ]
 
         logger.info(f"Creating card json for user: {len(user_prompt.split())}")
-        self.client.chat.completions.create(
+        return self.client.chat.completions.create(
             model=self.model,
-            response_format=self.response_format,
             messages=messages
         )
--- app/models/flashcard_models.py
+++ app/models/flashcard_models.py
@@ -8,17 +8,23 @@ class DifficultyEnum(Enum):
     hard = "hard"
 
 
-@dataclass(frozen=False, kw_only=True, slots=True)
-class FlashCard:
-    difficulty_level: DifficultyEnum
-    tags: list[str]
-    front_site: str
-    back_site: str
+class NonEmptyString(str):
+    def __new__(cls, value: str):
+        if not value.strip():
+            raise ValueError("String must be non-empty.")
+        return super().__new__(cls, value)
 
-    def __post_init__(self) -> None:
-        if not self.front_site:
-            raise ValueError("Flashcard has to have a question.")
-        if not self.back_site:
-            raise ValueError("Flashcard has to have an answer.")
-        if not self.tags:
-            raise ValueError("Tags must be a non-empty set.")
+
+@dataclass(frozen=False, kw_only=True, slots=True)
+class FlashCardSrc:
+    difficulty_level: DifficultyEnum
+    tags: list[NonEmptyString]
+    front_site: NonEmptyString
+    back_site: NonEmptyString
+    origin: NonEmptyString
+
+
+@dataclass(frozen=False, kw_only=True, slots=True)
+class FlashCardId(FlashCardSrc):
+    id: int
+
